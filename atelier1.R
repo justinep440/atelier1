f3d71680e7147c6863472dea45c6ed82a2d3e18e
justinep440 <56030679+justinep440@users.noreply.github.com>
2019-10-01 15:52:17 +0200

    Update atelier1.R

--- atelier1.R
+++ atelier1.R
@@ -3,16 +3,21 @@
 14*25
 -3/10
 4*sqrt(2)
-x<-2 
+x<-2 #ou x=2 ou 2->x
+sin(x)
+sin(2)
 y<-sin(x)
+exp(1)
+pi
+
 
 #Variables
 tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
 length(tailles)#taille echantillon
 mean(tailles) #moyenne
-sd(tailles) #écart-type
+sd(tailles) #Ã©cart-type
 var(tailles) #variance
-summary(tailles) #résumé
+summary(tailles) #rÃ©sumÃ©
 hist(tailles) #histogramme
 poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
 poids
@@ -25,4 +30,4 @@ tailles.m<-tailles/100
 tailles.m
 imc<-poids/tailles.m^2
 imc
-donnees<-data.frame(tailles,poids,imc) #data.frame = base de données -> avec les 3 variables
+donnees<-data.frame(tailles,poids,imc) #data.frame = base de donnÃ©es -> avec les 3 variables
